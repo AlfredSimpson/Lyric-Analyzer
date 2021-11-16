e3799f4a5cd3be0c28207c1835b3dcf0f13c8759
Al Simpson <73041922+AlfredSimpson@users.noreply.github.com>
2021-11-16 11:19:48 -0500

    Create LSA.py
    Set up basic formatting of contents of songs, removing punctuation and casing.

---
+++ LSA.py
@@ -0,0 +1,25 @@
+'''
+This is Version 1.0 of Alfred Simpson's Lyrics Sentiment Analysis program. 
+
+It relies on the user to input .txt files of lyrics from an artist. Future versions plan to crawl the internet based upon user input of artist name and album names or song names.
+'''
+import string
+
+def lyrics(inFile):
+	wordDict = {}
+	otherSum = 0
+	selfSum = 0
+	jointSum =0
+	formattedLyrics = []
+	inFile = open(inFile, 'r')
+	songs = inFile.readlines()
+	inFile.close()
+	for line in songs:
+		for word in lines:
+			word = word.strip(string.punctuation)
+			formattedLyrics += word
+			formattedLyrics += ''
+	formattedLyrics = formattedLyrics.split()
+
+
+	# Establishing words flagged for sentiment
